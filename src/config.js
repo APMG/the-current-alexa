5bc04c7c4c6e4da6fd16acbef7068485bbe75760
Eileen Noonan <enoonan@mpr.org>
2017-10-19 15:54:12 -0500

    update response for asking about show

--- src/config.js
+++ src/config.js
@@ -2,6 +2,8 @@ const stationSlug = 'the-current'
 const config = {
   APP_ID: 'amzn1.ask.skill.507b6759-42a6-4fc9-82b5-95043b7fee00',
   STATION_SLUG: stationSlug,
+  STATION_NAME: 'The Current',
+  DEFAULT_SHOW_NAME: 'Current Music',
   STREAM_URL: 'https://current.stream.publicradio.org/kcmp.mp3',
   NOW_PLAYING_URL: 'https://nowplaying.publicradio.org/' + stationSlug,
   CARD_TITLE: 'The Current Live Stream',
--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import config from './config'
 import mprAlexaBase from 'mpr-alexa-base'
+import get from 'lodash.get'
 
 let handlers = mprAlexaBase.getIntentHandlers(config)
 
 handlers['RequestSongIntent'] = function () {
-  this.emit(
-    ':tellWithLinkAccountCard',
-    'You need to link your APM account to this skill before you can request a song'
-  )
+  let token = get(this, 'event.session.user.accessToken')
+
+  if (!token) {
+    this.emit(
+      ':tellWithLinkAccountCard',
+      'You need to link your APM account to this skill before you can request a song'
+    )
+  }
+
+  this.emit(':tell', 'You are logged in. Let\'s have a little dialogue about this')
 }
 
 exports.handler = mprAlexaBase.createLambdaHandler(handlers)
