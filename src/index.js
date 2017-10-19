d08bddc4dd9e183f9f48672a552e7fee668ab892
Eileen Noonan <enoonan@mpr.org>
2017-10-19 11:57:06 -0500

    add test intent handler for requesting a song

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import config from './config'
 import mprAlexaBase from 'mpr-alexa-base'
 
-mprAlexaBase.setConfig(config)
-exports.handler = mprAlexaBase.createLambdaHandler()
+let handlers = mprAlexaBase.getIntentHandlers(config)
+
+handlers['RequestSongIntent'] = function () {
+  this.emit(
+    ':tellWithLinkAccountCard',
+    'You need to link your APM account to this skill before you can request a song'
+  )
+}
+
+exports.handler = mprAlexaBase.createLambdaHandler(handlers)
