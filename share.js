b0d08eb6d299dd4c420eb821aa31db6a15e1876e
Eileen Noonan <enoonan@mpr.org>
2018-05-14 13:04:45 -0500

    change name and description, add share file to export sharable intents

---
+++ share.js
@@ -0,0 +1,5 @@
+module.exports = {
+  getSongRating: require('./src/intents/get-song-rating'),
+  rateCurrentSong: require('./src/intents/rate-current-song'),
+  requestSong: require('./src/intents/request-song')
+}
